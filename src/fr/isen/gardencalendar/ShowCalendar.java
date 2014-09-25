fac2cb2219cd46d6d9095122799f9f5a24926c0b
françois dambrine <artragis@gmail.com>
2014-09-25 16:48:06 +0200

    calendar + spinner

--- src/fr/isen/gardencalendar/ShowCalendar.java
+++ src/fr/isen/gardencalendar/ShowCalendar.java
@@ -2,6 +2,9 @@ package fr.isen.gardencalendar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ShowCalendar extends Activity {
     /**
@@ -11,5 +14,12 @@ public class ShowCalendar extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_LONG);
+        Spinner plantList = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(this, R.array.plants_array, android.R.layout.simple_spinner_dropdown_item);
+
+        plantList.setAdapter(list);
+        plantList.setClickable(true);
+
     }
 }
